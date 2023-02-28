45cd7bf5aabcac810d168714c739980ada683f3a
letsummer <letsummerparty@gmail.com>
2023-02-28 10:00:57 +0900

    230228 index and enything upload

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../comoponents/Header';
+import Body from '../comoponents/Body';
+
+function Home(){
+    return(
+        <div>
+            <Header/>
+            <Body />
+        </div>
+    );
+}
+
+export default Home;
