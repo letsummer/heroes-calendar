9891418c5cc86c1fe65fb48745530e61773e3b2a
letsummer <letsummerparty@gmail.com>
2023-02-28 12:19:36 +0900

    230228 Header.js 틀 작성

--- src/comoponents/Header.js
+++ src/comoponents/Header.js
@@ -5,10 +5,22 @@ function Header(){
     return(
         <div className={styles.header}>
             <span>T O D A Y</span>
-            <div>
-                <span>홈 vs 원정</span>
-                <span>장소</span>
-                <span>날씨</span>
+            <div id={styles.todayInfo}>
+                <div id={styles.todayInfo__some}>
+                    <p>something</p>
+                </div>
+                <div id={styles.todayInfo__play}>
+                    <div id={styles.todayInfo__team}>
+                        <span></span>
+                        <span> vs </span>
+                        <span></span>
+                    </div>
+                    <div>18:30</div>
+                </div>
+                <div id={styles.todayInfo__stadium}>
+                    <p>구장</p>
+                    <p>날씨</p>
+                </div>
             </div>
         </div>
     );
